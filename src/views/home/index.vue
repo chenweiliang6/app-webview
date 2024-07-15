3afbd67ba00073afee660aa5c65659c75d85fdfd
19994643173 <1075005842@qq.com>
2024-07-15 19:51:45 +0800

    '完成点击滑动'

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,28 +1,27 @@
 <template>
   <transition name="van-fade">
     <div class="index">
-      <van-sticky>
-        <div class="navbar">
-          <span class="logo">中建民筑</span>
-          <van-popover v-model:show="showPopover" :actions="actions" @select="onSelect">
-            <template #reference>
-              <van-icon name="wap-nav" color="#fff" />
-            </template>
-          </van-popover>
-        </div>
-      </van-sticky>
+      <div class="navbar">
+        <span class="logo">中建民筑</span>
+        <van-popover v-model:show="showPopover" :actions="actions" placement="left-start" @select="onSelect"
+          class="popover">
+          <template #reference>
+            <van-icon name="wap-nav" color="#fff" />
+          </template>
+        </van-popover>
+      </div>
       <div class="index-container">
-        <Aboutus></Aboutus>
-        <Preface></Preface>
-        <Contents></Contents>
-        <Background></Background>
-        <Development></Development>
-        <housingReform></housingReform>
-        <reformGateway></reformGateway>
-        <Trends></Trends>
-        <aboutEnterprise></aboutEnterprise>
-        <ourHonor></ourHonor>
-        <lastPage></lastPage>
+        <Aboutus class="index-container-page"></Aboutus>
+        <Preface class="index-container-page"></Preface>
+        <Contents class="index-container-page"></Contents>
+        <Background class="index-container-page"></Background>
+        <Development class="index-container-page"></Development>
+        <housingReform class="index-container-page"></housingReform>
+        <reformGateway class="index-container-page"></reformGateway>
+        <Trends class="index-container-page"></Trends>
+        <aboutEnterprise class="index-container-page"></aboutEnterprise>
+        <ourHonor class="index-container-page"></ourHonor>
+        <lastPage class="index-container-page"></lastPage>
       </div>
     </div>
   </transition>
@@ -40,7 +39,7 @@ import Trends from './components/trends.vue'
 import aboutEnterprise from './components/about-enterprise.vue'
 import ourHonor from './components/our-honor.vue'
 import lastPage from "./components/last-page.vue";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const showPopover = ref(false)
 const actions = ref([
@@ -56,6 +55,34 @@ const actions = ref([
   { text: '我们的荣誉' },
   { text: '关于中建民筑' },
 ])
+const topList = ref([])
+
+const getHeight = () => {
+  const query = document.querySelectorAll('.index-container-page')
+  query.forEach((item, index) => {
+    let top = index > 0 ? topList.value[index - 1] : 0;
+    top += item.clientHeight;
+    topList.value.push(top);
+  })
+}
+
+const onSelect = (action, index) => {
+  if (index == 0) {
+    document.querySelector('.index-container').scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  } else {
+    document.querySelector('.index-container').scrollTo({
+      top: topList.value[index - 1],
+      behavior: 'smooth'
+    })
+  }
+}
+
+onMounted(() => {
+  getHeight()
+})
 
 </script>
 
@@ -64,6 +91,7 @@ const actions = ref([
   width: 100%;
   height: 100%;
   overflow: hidden;
+
   .navbar {
     background: rgba(0, 0, 0, 0.9);
     display: flex;
@@ -71,6 +99,7 @@ const actions = ref([
     align-items: center;
     padding: 0 10px;
     height: 30px;
+
     .logo {
       color: transparent;
       background-image: linear-gradient(to bottom, #fff2cc, #ffdd77);
@@ -78,10 +107,19 @@ const actions = ref([
       font-family: hwxk;
     }
   }
+
   .index-container {
     width: 100%;
     height: 100%;
     overflow: auto;
+  }
+}
+
+.popover {
+  ::v-deep .van-popover__content {
+    .van-popover__action {
+      width: 200px;
+    }
   }
 }
 </style>
