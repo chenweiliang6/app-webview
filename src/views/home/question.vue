3ddf34f5dccaa869a8e6b93ffdb0cc1a7e97029e
19994643173 <1075005842@qq.com>
2025-03-29 16:35:41 +0800

    :fix 提交dist

--- src/views/home/question.vue
+++ src/views/home/question.vue
@@ -1,13 +1,14 @@
 <template>
-<div>
-
-</div>
+  <div>
+    <van-button @click="toFeedback">新建反馈</van-button>
+  </div>
 </template>
 
 <script setup>
-
+const toFeedback = () => {
+    window.location.href = 'https://gitee.com/CWLcwl0219/William-Player/issues';
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
